640f802fe84bc6dbdc27d7d5c6bb44cf6d8eee98
FabianDL <fabian,drehmel@gmail.com>
2019-11-15 11:31:50 +0100

    refactor

--- Übung5/Drehmel_Fabian_Uebung5/Teilnehmer.cpp
+++ Übung5/Drehmel_Fabian_Uebung5/Teilnehmer.cpp
@@ -11,7 +11,7 @@ void Teilnehmer::lerntKennen(Teilnehmer &person) {
         bekannte.push_back(person);
         person.lerntKennen(*this); //die andere Person bekommt diese hier auch
     } else {
-        bool nochNichtInListe = true;
+        bool nochNichtInListe  = true;
         for (size_t i = 0; i < bekannte.size(); i++) {
             if (person.compare(bekannte.at(i))) {
                 nochNichtInListe = false;
@@ -28,9 +28,6 @@ std::vector<Teilnehmer> Teilnehmer::druckeBekannte() {
     for (auto &i : bekannte) {
         std::cout << i.gibNamen() << ", ";
     }
-//    for (size_t i = 0; i < bekannte.size(); i++) {
-//        std::cout << bekannte.at(i).name;
-//    }
     std::cout << std::endl;
 }
 
